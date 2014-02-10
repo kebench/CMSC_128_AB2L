e4c49624b0fea107f29f98fc26f0606bdfad7db8
cjpvillaruel <cjpvillaruel@gmail.com>
2014-02-10 16:52:37 +0800

    Removed duplicate files

--- ics_library_ab2l/application/views/admin/view_admin_header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html><head>
-
-	<script src="<?php echo  base_url() ?>js/validation.js" type="text/javascript"></script>
-</head>
-
-<body>
--- ics_library_ab2l/application/views/user/view_not_logged (2).php
+++
@@ -1,9 +0,0 @@
-
-				<li><a href="<?php echo base_url(); ?>index.php/user/controller_login">Login</a></li>
-				<li><a href="<?php echo base_url(); ?>index.php/user/controller_register">Sign-up</a></li>
-			</ul>
-		</nav>
-
-			</div>
-	</aside>
-</div>
