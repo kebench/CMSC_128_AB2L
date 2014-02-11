c0b04e54dc3d8ca91af8f001cf0c75ff47518bfd
edelsmartvaldellon <edelweisAV@gmail.com>
2014-02-10 17:28:01 -0800

    Changed controller_book for viewing all books in user

--- ics_library_ab2l/application/controllers/user/controller_books.php
+++ ics_library_ab2l/application/controllers/user/controller_books.php
@@ -4,14 +4,20 @@ class Controller_books extends CI_Controller {
     function index() {
         $this->load->helper(array('form','html'));
         $this->load->view("user/view_header");
-        $this->load->view("user/view_books");
+        $this->load->model("model_get_list");
+        $data['result'] = $this->model_get_list->all_books();
+        $this->load->view("user/view_all_books",$data);
         $this->load->view("user/view_navigation");
+    
+
         if($this->session->userdata('logged_in')){
             $this->load->view("user/view_logged_in");
         }
         else{
              $this->load->view("user/view_not_logged");
         }  
+        // $this->load->view("user/view_all_books",$data);
+
         $this->load->view("user/view_footer");
     }
 }
