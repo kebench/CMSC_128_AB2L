c435c4b0f4c4f95513705b0b15bb3343a5d934dd
odyzzadaleon <odyzzadaleon@gmail.com>
2014-02-26 20:40:34 -0800

    Updated session user side only

--- ics_library_ab2l/application/config/database.php
+++ ics_library_ab2l/application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'password';
+$db['default']['username'] = 'ab2l';
+$db['default']['password'] = 'cmsc128ab2l';
 $db['default']['database'] = 'ics_library';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
