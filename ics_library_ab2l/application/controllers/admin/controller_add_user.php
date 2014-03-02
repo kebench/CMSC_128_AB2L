d2360a0ca5ed4f8ecdf2b7e7162012c42b043986
cjpvillaruel <cjpvillaruel@gmail.com>
2014-03-02 21:21:55 +0800

    Fixed add user in admin page.

--- ics_library_ab2l/application/controllers/admin/controller_add_user.php
+++ ics_library_ab2l/application/controllers/admin/controller_add_user.php
@@ -26,20 +26,21 @@ class Controller_add_user extends CI_Controller {
               redirect('index.php/user/controller_login', 'refresh');
             $this->load->library('form_validation');
             // field name, error message, validation rules
-            $this->form_validation->set_rules('fname', 'First Name', 'trim|required|alpha|xss_clean');
-            $this->form_validation->set_rules('minit', 'Middle Initial', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('lname', 'Last Name', 'trim|required|alpha|xss_clean');
+           $this->form_validation->set_rules('fname', 'First Name', 'trim|required|alpha|xss_clean');
+          $this->form_validation->set_rules('minit', 'Middle Initial', 'trim|required|xss_clean');
+          $this->form_validation->set_rules('lname', 'Last Name', 'trim|required|alpha|xss_clean');
 
-             $this->form_validation->set_rules('stdNum', 'Student Number', 'trim|required|min_length[10]|alpha_dash|xss_clean');
-            $this->form_validation->set_rules('college', 'College', 'trim|min_length[3]|alpha|xss_clean');
-            $this->form_validation->set_rules('course', 'Course', 'trim|min_length[3]|xss_clean');
-            $this->form_validation->set_rules('classi', 'Classification', 'trim|alpha|xss_clean');
-            
-            $this->form_validation->set_rules('eadd', 'Your Email', 'trim|required|valid_email');
+           $this->form_validation->set_rules('stdNum', 'Student Number', 'trim|required|min_length[10]|alpha_dash|xss_clean');
+          $this->form_validation->set_rules('college', 'College', 'trim|min_length[2]|alpha|xss_clean');
+          $this->form_validation->set_rules('course', 'Course', 'trim|min_length[3]|xss_clean');
+          $this->form_validation->set_rules('classi', 'Classification', 'trim|alpha|xss_clean');
+          
+          $this->form_validation->set_rules('eadd', 'Your Email', 'trim|required|valid_email');
 
-            $this->form_validation->set_rules('uname', 'Username', 'trim|required|min_length[4]|alpha_dash|xss_clean');
-            $this->form_validation->set_rules('pass', 'Password', 'trim|required|min_length[5]|max_length[32]|alpha_numeric');
-            $this->form_validation->set_rules('cpass', 'Password Confirmation', 'trim|required|matches[pass]');
+          $this->form_validation->set_rules('uname', 'Username', 'trim|required|min_length[4]|xss_clean|callback_usernameRegex');
+          $this->form_validation->set_rules('pass', 'Password', 'trim|required|min_length[5]|max_length[32]|alpha_numeric');
+          $this->form_validation->set_rules('cpass', 'Password Confirmation', 'trim|required|matches[pass]');
+
 
             if($this->form_validation->run() == FALSE)
             {
@@ -52,6 +53,15 @@ class Controller_add_user extends CI_Controller {
               $data['msg'] = "You successfully registered an account. You may proceed to ICS library to activate it! ";
               $this->success($data);
             }
+          }
+    }
+
+     public function username_Regex($username){
+        if (preg_match('/^[A-Za-z][A-Za-z0-9._]{4,20}$/', $username) ) {
+            return TRUE;
+          } else {
+            return FALSE;
+            $this->form_validation->set_message('username_Regex', 'Invalid input.');
           }
     }
 
